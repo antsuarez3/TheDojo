e0014b432dc72905fd33d968f9a3b196e7e181ba
Anthony Suarez <antsuarez305@gmail.com>
2022-06-18 18:10:45 -0500

    Fetch a single project

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import Select from 'react-select'
 import { useCollection } from '../../hooks/useCollection'
+import { timestamp } from '../../firebase/config'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useFirestore } from '../../hooks/useFirestore'
 
 // styles
 import './Create.css'
+import { useNavigate } from 'react-router-dom'
 
 const categories = [
   { value: 'development', label: 'Development' },
@@ -14,8 +18,11 @@ const categories = [
 ]
 
 export default function Create() {
+  const navigate = useNavigate()
+  const { addDocument, response } = useFirestore('projects')
   const { documents } = useCollection('users')
   const [users, setUsers] = useState([])
+  const { user } = useAuthContext()
 
   // form field values
   const [name, setName] = useState('')
@@ -23,6 +30,7 @@ export default function Create() {
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
 
   useEffect(() => {
     if (documents) {
@@ -33,9 +41,47 @@ export default function Create() {
     }
   }, [documents])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(name, details, dueDate, category.value, assignedUsers)
+    setFormError(null)
+
+    if (!category) {
+      setFormError('Please select a project category')
+      return
+    }
+
+    if (assignedUsers.length < 1) {
+      setFormError('Please assign the project to at least 1 user')
+      return
+    }
+
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid,
+    }
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id,
+      }
+    })
+
+    const project = {
+      name,
+      details,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy,
+      assignedUsersList,
+    }
+
+    await addDocument(project)
+    if (!response.error) {
+      navigate('/')
+    }
   }
 
   return (
@@ -86,6 +132,8 @@ export default function Create() {
         </label>
 
         <button className='btn'>Add Project</button>
+
+        {formError && <p className='error'>{formError}</p>}
       </form>
     </div>
   )
